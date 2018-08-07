e8bae508ed39e81d7d164679e453b0d603de2101
Steve Van Opstal <stevevanopstal@gmail.com>
2018-08-07 14:11:22 +0200

    intial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./app.component.css'],
+  template: `
+   <div class="content">
+   </div>`
+})
+export class AppComponent {
+}
