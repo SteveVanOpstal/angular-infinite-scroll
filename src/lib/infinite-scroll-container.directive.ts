b7c9859fc11c585dca1f1d0d9edbc8b15fd2a66e
Steve Van Opstal <stevevanopstal@gmail.com>
2018-09-03 15:11:04 +0200

    refactor(container): incorrect class naming

--- src/lib/infinite-scroll-container.directive.ts
+++ src/lib/infinite-scroll-container.directive.ts
@@ -2,8 +2,8 @@ import {CdkScrollable, ScrollDispatcher} from '@angular/cdk/scrolling';
 import {Directive, ElementRef, NgZone} from '@angular/core';
 
 @Directive({selector: '[infiniteScrollContainer]'})
-export class infiniteScrollContainerDirective extends CdkScrollable {
-  constructor(_elementRef: ElementRef, _scroll: ScrollDispatcher, _ngZone: NgZone) {
-    super(_elementRef, _scroll, _ngZone);
+export class InfiniteScrollContainerDirective extends CdkScrollable {
+  constructor(elementRef: ElementRef, scroll: ScrollDispatcher, ngZone: NgZone) {
+    super(elementRef, scroll, ngZone);
   }
 }
--- src/lib/infinite-scroll.module.ts
+++ src/lib/infinite-scroll.module.ts
@@ -1,17 +1,17 @@
 import {ScrollDispatchModule} from '@angular/cdk/scrolling';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {infiniteScrollContainerDirective} from 'angular-infinite-scroll/infinite-scroll-container.directive';
+import {InfiniteScrollContainerDirective} from 'angular-infinite-scroll/infinite-scroll-container.directive';
 
 import {InfiniteScrollComponent, InfiniteStaticMarker, InfiniteTemplateMarker} from './infinite-scroll.component';
 import {InfiniteScrollDirective} from './infinite-scroll.directive';
 
 @NgModule({
   declarations:
-      [InfiniteScrollDirective, InfiniteScrollComponent, infiniteScrollContainerDirective, InfiniteStaticMarker, InfiniteTemplateMarker],
+      [InfiniteScrollDirective, InfiniteScrollComponent, InfiniteScrollContainerDirective, InfiniteStaticMarker, InfiniteTemplateMarker],
   imports: [CommonModule, ScrollDispatchModule],
   exports:
-      [InfiniteScrollDirective, InfiniteScrollComponent, infiniteScrollContainerDirective, InfiniteStaticMarker, InfiniteTemplateMarker]
+      [InfiniteScrollDirective, InfiniteScrollComponent, InfiniteScrollContainerDirective, InfiniteStaticMarker, InfiniteTemplateMarker]
 })
 export class InfiniteScrollModule {
 }
